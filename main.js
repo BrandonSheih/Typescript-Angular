60625d07b49aef3fba2cb9c91a8ff62014026308
BrandonSheih <44211623+BrandonSheih@users.noreply.github.com>
2019-07-11 19:32:34 -0700

    finished a bried summary of typescript topics

--- main.js
+++ main.js
@@ -1,2 +1,119 @@
-var message = 'Hello World';
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+//let variables don't need to be declared
+//const variables have to be declared
+//any variables means it can be anything
+//cannot access, call, or change a variable of type unknown
+//too access, call, or change a variable of type unknown you need to do ' as string' or ' as number' etc
+var message = 'Welcome back';
 console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = 'Codevolution';
+var isBeginner = true;
+var total = 0;
+var name = 'Brandon';
+var sentence = "My name is " + name + "\nI am a beginner in Typescript";
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['chris', 22];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Brandon';
+var myVariable = 10;
+function hasName(obj) {
+    return !!obj &&
+        typeof obj === "object" &&
+        "name" in obj;
+}
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+//(myVariable as string).toUpperCase();
+var a;
+a = 10;
+a = true;
+var b = 20;
+var multiType;
+multiType = 20;
+multiType = true;
+var anyType;
+anyType = 20;
+anyType = true;
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+add(5, 10);
+add(5);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+fullName(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Brandons');
+console.log(emp1.employeeName);
+emp1.greet();
+//inheritance
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
+//access modifers
+//by default variables are public
+//public employeeName: string -> public
+//private employeeName: string -> means the variable is only visible in the class it is made
+//protected employeeName: string -> means the variable is visible to all classes that inherit variable
